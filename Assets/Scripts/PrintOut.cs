83742c98cf77067e1067c04be7450df5476a1cf8
Juunicacio <juhnicacio.bio@gmail.com>
2021-11-15 17:43:04 +0100

    thinking about fly game

---
+++ Assets/Scripts/PrintOut.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintOut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if you attach this script to one game obj it will print out the message bellow
+        Debug.Log("I'm the first building in the front view");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
