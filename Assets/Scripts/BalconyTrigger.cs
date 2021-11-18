6db7ffeb6553ea306034b0baa5f6e1baede835eb
Juunicacio <juhnicacio.bio@gmail.com>
2021-11-18 19:23:34 +0100

    timer to enter in the building working

--- Assets/Scripts/BalconyTrigger.cs
+++ Assets/Scripts/BalconyTrigger.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class BalconyTrigger : MonoBehaviour
 {
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    //Debug.Log(other.gameObject.tag);
-    //    if (other.gameObject.tag == "Player")
-    //    {
-    //        Debug.Log("Press F to enter in this apartment");
-    //    }
-    //}
+    bool TimerStarted = false;
+    float TimeCounter = 0f;
+    float TimeToCount = 1f;
+    bool GuiToEnterIsOn = false;
+    bool GuiShowed = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log($"After {TimeToCount} seconds in front this apt you can enter in it.");
+            if (!TimerStarted) TimerStarted = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (TimerStarted)
+        {
+            TimeCounter += Time.deltaTime;
+            //Debug.Log($"TimeCounter: {TimeCounter}");
+        }
+        if (TimeCounter >= TimeToCount & !GuiShowed)
+        {
+            GuiToEnterIsOn = true;
+            GuiShowed = true;
+            Debug.Log("Press F to enter in this apartment");
+            Debug.Log($"GuiShowed status = {GuiShowed}");
+        }
+    }
 
     //FunctionTimer functionTimerVar;
 
-    
+    /*
     private void OnTriggerStay(Collider other)
     {
         //Debug.Log(other.gameObject.tag);
@@ -27,24 +51,30 @@ public class BalconyTrigger : MonoBehaviour
             // let's store this function in a variable 
             /*functionTimerVar = new FunctionTimer(WannaEnterInThisBalcony, 0.02f);
             functionTimerVar.TimerUpdate();*/
-            FunctionTimer.CreateFunctionAfterTime(WannaEnterInThisBalcony, 0.02f, "Timer 1 second");
-            //FunctionTimer.CreateFunctionAfterTime(WannaEnterInThisBalcony, 0.5f);
-        }
-    }   
-
+    /*FunctionTimer.CreateFunctionAfterTime(WannaEnterInThisBalcony, 0.02f, "Timer 1 second");
+    //FunctionTimer.CreateFunctionAfterTime(WannaEnterInThisBalcony, 0.5f);
+}
+}   */
+    /*
     // Create an action to occurr after 5 seconds in front of the balcony window
     void WannaEnterInThisBalcony()
     {
         //Debug.Log("You stayed 5 seconds in here, would you like enter in this apartment?");
         Debug.Log("Would you like enter in this apartment?");
     }
+    */
 
     // I would then also reset if player leaves the collider
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            FunctionTimer.StopTimer("Timer 1 second");
+            //FunctionTimer.StopTimer("Timer 1 second");
+            TimerStarted = false;
+            TimeCounter = 0;
+            GuiToEnterIsOn = false;
+            Debug.Log("You can no more enter in this one");
+            Debug.Log($"GuiShowed status = {GuiShowed}");
         }
     }
 }
